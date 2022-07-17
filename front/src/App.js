4b5a0054a83a8b0edad45d3f08254d5263e504c0
m4t1ce <m4t1ce@gmail.com>
2022-07-17 13:54:37 +0200

    Bulma instalation

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			{/* log/register
+			
+
+
+			CLIENT:
+			SHOW ORDER:
+			OrderHistory - remove order
+			ADD ORDER:
+			-order data
+			WORKER:
+			SHOW ORDER:
+			OrderList - take order
+			OrderHistory
+
+			*/}
+			<div></div>
+		</div>
+	);
 }
 
 export default App;
