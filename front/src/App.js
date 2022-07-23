dc8ad2f932211c691f84bdfac307e126ff5b96b0
m4t1ce <m4t1ce@gmail.com>
2022-07-23 15:14:56 +0200

    Implemented protected routes

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,10 @@ import { Landing } from "./Makro/Landing";
 import { Login } from "./Makro/Login";
 import { Register } from "./Makro/Register";
 import { Navigation } from "./Mikro/Navigation";
+import { ProtectedRoute } from "./Mikro/ProtectedRoute";
+import { useSelector } from "react-redux";
 function App() {
+	const { user } = useSelector((state) => state.auth);
 	return (
 		<>
 			<Router>
@@ -17,8 +20,23 @@ function App() {
 						<Route path="/" element={<Landing />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/register" element={<Register />} />
-						<Route path="/Client" element={<Client />} />
-						<Route path="/Worker" element={<Worker />} />
+
+						<Route
+							path="/Client"
+							element={
+								<ProtectedRoute user={user}>
+									<Client />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/Worker"
+							element={
+								<ProtectedRoute user={user}>
+									<Worker />
+								</ProtectedRoute>
+							}
+						/>
 					</Routes>
 				</div>
 			</Router>
