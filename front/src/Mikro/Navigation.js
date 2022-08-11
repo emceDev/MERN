a00b0233f92a5998004eb931e7e422585a15c9e0
m4t1ce <m4t1ce@gmail.com>
2022-08-11 15:20:56 +0200

    impl:hamburger menu in navbar

--- front/src/Mikro/Navigation.js
+++ front/src/Mikro/Navigation.js
@@ -1,9 +1,10 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, reset } from "../features/auth/authSlice";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export const Navigation = () => {
+	const [active, setActive] = useState(false);
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const { user } = useSelector((state) => state.auth);
@@ -14,30 +15,61 @@ export const Navigation = () => {
 	}
 
 	return (
-		<div>
-			<Link to="/">
-				<button class="button">HomePage</button>
-			</Link>
-			<Link to="/worker">
-				<button class="button">Worker</button>
-			</Link>
-			<Link to="/client">
-				<button class="button">Client</button>
-			</Link>
-			{user ? (
-				<button class="button" onClick={() => logOut()}>
-					Logout
-				</button>
-			) : (
-				<>
-					<Link to="/login">
-						<button class="button">Login</button>
-					</Link>
-					<Link to="/register">
-						<button class="button">Register</button>
-					</Link>
-				</>
-			)}
+		<div className="navbar is-tab">
+			<div class="navbar-brand">
+				<a
+					role="button"
+					className={active ? "navbar-burger is-active" : "navbar-burger"}
+					aria-label="menu"
+					aria-expanded="false"
+					onClick={() => setActive(!active)}
+				>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</a>
+			</div>
+			<div className={active ? "navbar-menu is-active" : "navbar-menu"}>
+				<NavLinks user={user} logout={() => logOut()} />
+			</div>
 		</div>
+	);
+};
+
+const NavLinks = (props) => {
+	return (
+		<>
+			<div className="navbar-start">
+				<Link to="/" className="navbar-item">
+					<button class="button">HomePage</button>
+				</Link>
+
+				<Link to="/worker" className="navbar-item">
+					<button class="button">Worker</button>
+				</Link>
+
+				<Link to="/client" className="navbar-item">
+					<button class="button">Client</button>
+				</Link>
+			</div>
+			<div className="navbar-end">
+				{props.user ? (
+					<div className="navbar-item">
+						<button class="button" onClick={() => props.logout()}>
+							Logout
+						</button>
+					</div>
+				) : (
+					<>
+						<Link to="/login" className="navbar-item">
+							<button class="button">Login</button>
+						</Link>
+						<Link to="/register" className="navbar-item">
+							<button class="button">Register</button>
+						</Link>
+					</>
+				)}
+			</div>
+		</>
 	);
 };
